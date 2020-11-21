4b14e0cf3535178c1219506ff3465c7c7771f7ea
harut <harutiro2027@outlook.jp>
2020-11-21 19:58:45 +0900

    vor0.11 テキスト途中 レイアー完成 メイン関数の作成

--- app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
@@ -2,10 +2,24 @@ package app.makino.harutiro.anzan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val number1Text = findViewById<TextView>(R.id.number1Text)
+        val number2Text = findViewById<TextView>(R.id.number2Text)
+
+
+        val randomNumber1 = Random.nextInt(1000)
+        val randomNumber2 = Random.nextInt(1000)
+
+        number1Text.text = randomNumber1.toString()
+        number2Text.text = randomNumber2.toString()
     }
 }
