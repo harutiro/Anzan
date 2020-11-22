f720cb8ed4a954c094d70dd777b227610a12dff3
harutiro <harutiro2027@outlook.jp>
2020-11-22 12:59:50 +0900

    vor 0.12 テキスト途中 乱数から文字を出力まで 2つ目の画面へ

--- app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val number1Text = findViewById<TextView>(R.id.number1Text)
         val number2Text = findViewById<TextView>(R.id.number2Text)
+        val signText :TextView = findViewById<TextView>(R.id.signText)
 
 
         val randomNumber1 = Random.nextInt(1000)
@@ -21,5 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         number1Text.text = randomNumber1.toString()
         number2Text.text = randomNumber2.toString()
+
+        val randomOperator = Random.nextInt(2)
+        var operatorText = ""
+        var correctAnswer = 0
+
+        if(randomOperator == 0 ){
+
+            operatorText = "+"
+            signText.text = operatorText
+            correctAnswer = randomNumber1 + randomNumber2
+
+        }else{
+
+            operatorText = "-"
+            signText.text = operatorText
+            correctAnswer = randomNumber1 - randomNumber2
+
+        }
     }
+
+
+
 }
