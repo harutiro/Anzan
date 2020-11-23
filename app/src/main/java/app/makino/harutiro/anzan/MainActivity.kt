f27b7f11127ca9d764e5dbe83e81f06f57386c87
harutiro <harutiro2027@outlook.jp>
2020-11-23 18:01:36 +0900

    vor 0.13 テキスト途中 メイン画面から答え合わせの画面に飛ぶようになった 文字入力するところはEditTextというデータ型
    次回アンサーのクラス作成

--- app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/anzan/MainActivity.kt
@@ -1,7 +1,10 @@
 package app.makino.harutiro.anzan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -14,7 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         val number1Text = findViewById<TextView>(R.id.number1Text)
         val number2Text = findViewById<TextView>(R.id.number2Text)
-        val signText :TextView = findViewById<TextView>(R.id.signText)
+        val signText = findViewById<TextView>(R.id.signText)
+        val checkButton  = findViewById<Button>(R.id.cheakButton)
+        val inputText= findViewById<EditText>(R.id.inputText)
 
 
         val randomNumber1 = Random.nextInt(1000)
@@ -25,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         val randomOperator = Random.nextInt(2)
         var operatorText = ""
-        var correctAnswer = 0
+        var correctAnswer: Int = 0
 
         if(randomOperator == 0 ){
 
@@ -40,6 +45,26 @@ class MainActivity : AppCompatActivity() {
             correctAnswer = randomNumber1 - randomNumber2
 
         }
+
+        checkButton.setOnClickListener{
+            val yourAnswer = inputText.text.toString()
+
+            if(yourAnswer.isNotEmpty()){
+                val answerPage = Intent (this, AnswerActivity::class.java)
+                val questionText = randomNumber1.toString() + operatorText + randomNumber2.toString() +" ="
+
+                answerPage.putExtra("question",questionText)
+                answerPage.putExtra("answer",yourAnswer)
+                answerPage.putExtra("correct",correctAnswer.toString())
+
+                startActivity(answerPage)
+                finish()
+
+            }
+
+        }
+
+
     }
 
 
